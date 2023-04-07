d586b8e6c0d984b8daf0c67ae2cf7054316157d4
tekkamelon <type2guntank@gmail.com>
2023-04-07 14:56:49 +0900

    カーソルライン及びターミナルの設定の追加
    カーソルラインを強制的にアンダーラインに設定
    ターミナルのキーバインド及び行番号の非表示設定

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,7 +15,28 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 -- カーソルラインを表示
-vim.opt.cursorline= true
+vim.opt.cursorline = true
+
+-- カーソルラインの設定
+vim.cmd([[
+
+	" 一度カーソルラインをリセット
+	hi clear CursorLine
+
+	" カーソルラインをアンダーラインに設定
+	hi CursorLine gui=underline cterm=underline
+
+	" ターミナル起動時に行番号を非表示
+	autocmd TermOpen * setlocal norelativenumber
+	autocmd TermOpen * setlocal nonumber
+
+]])
+
+-- ターミナルノーマルモードへの移行
+vim.keymap.set('t', '<C-w><C-n>' , [[<C-\><C-n>]] , {noremap=true})
+
+-- swapファイルを別ディレクトリに作成
+vim.opt.directory = '/tmp'
 
 -- 分割方向を下と右
 vim.opt.splitbelow = true
