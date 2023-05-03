eb7aae3aa24e62113893af40493438c8c9e97cc4
tekkamelon <type2guntank@gmail.com>
2023-05-04 02:19:04 +0900

    Revert ""init.lua"の分割"
    起動速度が低下したため取り消し
    This reverts commit cbf7400e30fd5e147f8a0b74797e2c8c0d7a91cc.

--- .config/nvim/after/plugin/after_set.lua
+++ .config/nvim/after/plugin/after_set.lua
@@ -1,7 +1,6 @@
 -- after_set.lua
 -- プラグインの起動後に読込
 
-
 -- ====== 共通の設定 ======
 -- ポップアップメニューの設定
 vim.api.nvim_set_hl(0, 'Pmenu' , { guifg=white , guibg=grey})
@@ -11,13 +10,6 @@ vim.api.nvim_create_autocmd('FileType' , {pattern = '*' , command = 'setlocal fo
 
 -- カーソルラインをアンダーラインに設定
 vim.api.nvim_set_hl(0, 'CursorLine' , { underline = true })
-
--- ヤンクした範囲のハイライト,ビジュアルモード時にオフ
-vim.cmd([[
-
-	au TextYankPost * silent! lua vim.highlight.on_yank {higroup = "IncSearch", timeout = 700 , on_visual = false}
-
-]])
 -- ====== 共通の設定ここまで ======
 
 
