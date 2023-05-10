aa091d276b65b1bf7e33ee8e7819bcbaccef7301
tekkamelon <type2guntank@gmail.com>
2023-05-11 02:54:34 +0900

    vscode-neovimの設定
    vscode-neovim使用時のキーマップを別ファイルに分離

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -159,32 +159,8 @@ vim.g.mapleader = " "
 -- vscode-neovimから起動した際に真,それ以外で偽
 if vim.g.vscode then
 
-	-- 真の場合のキーマップ
-	vim.cmd([[
-		
-		" 保存,終了
-		nnoremap <silent> <leader>w :call VSCodeCall('workbench.action.files.save')<CR>
-		nnoremap <silent> <leader>W :call VSCodeCall('workbench.action.files.saveALL')<CR>
-		nnoremap <silent> <leader>q :call VSCodeCall('workbench.action.closeActiveEditor')<CR>
-		nnoremap <silent> <leader>Q :call VSCodeCall('workbench.action.closeAllEditors')<CR>
-
-		" バッファの切り替え
-		nnoremap <silent> <leader>j :call VSCodeCall('workbench.action.previousEditor')<CR>
-		nnoremap <silent> <leader>k :call VSCodeCall('workbench.action.nextEditor')<CR>
-
-		" ファイルを開く
-		nnoremap <silent> <leader>ff :call VSCodeCall('workbench.action.files.openFile')<CR>
-
-		" フォルダーを開く
-		nnoremap <silent> <leader>fh :call VSCodeCall('workbench.action.files.openFolder')<CR>
-
-		" ターミナルのトグル
-		nnoremap <silent> <leader>tt :call VSCodeCall('workbench.action.terminal.toggleTerminal')<CR>
-
-		" コメントのトグル
-		nnoremap <silent> <leader>g :call VSCodeCall('editor.action.commentLine')<CR>
-
-	]])
+	-- 真の場合は"vscode-neovim_set"の読み込み
+	require('vscode-neovim_set')
 
 else
 
