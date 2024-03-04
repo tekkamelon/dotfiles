e2f3dda2b418b8387a4cf45d3a0fad8813387fb9
tekkamelon <type2guntank@gmail.com>
2024-03-05 00:10:46 +0900

    軽微な修正
    vim script部分をluaに書き換え

--- .config/nvim/after/plugin/after_set.lua
+++ .config/nvim/after/plugin/after_set.lua
@@ -1,27 +1,33 @@
 -- after_set.lua
 -- プラグインの起動後に読込
 
+
 -- ====== 共通の設定 ======
 -- ポップアップメニューの設定
-vim.api.nvim_set_hl(0, 'Pmenu' , { guifg=white , guibg=grey})
+vim.api.nvim_set_hl(0, 'Pmenu',{ guifg=white , guibg=grey})
 
 -- 改行時の自動コメントアウトをoff
 vim.api.nvim_create_autocmd('FileType' , {pattern = '*' , command = 'setlocal formatoptions-=ro',})
 
 -- カーソルラインをアンダーラインに設定
 vim.api.nvim_set_hl(0, 'CursorLine' , { underline = true })
+
+-- ヤンクした範囲のハイライト,ビジュアルモード時にオフ
+vim.cmd([[
+
+	au TextYankPost * silent! lua vim.highlight.on_yank {higroup = "IncSearch", timeout = 700 , on_visual = false}
+
+]])
 -- ====== 共通の設定ここまで ======
 
 
 -- ポップアップメニューの設定,ホスト名を確認,"pop-os"であれ真,それ以外で偽
 if vim.fn.hostname() == "pop-os" then
 
-	-- 真の場合の設定
-	vim.cmd([[highlight PmenuSel guifg=#2f3234 guibg=#8389a3]])
+	vim.api.nvim_set_hl(0, 'PmenuSel',{ fg='#2f3234', bg='#8389a3' })
 
 else
 
-	-- 偽の場合の設定
-	vim.cmd([[highlight PmenuSel guifg=#2f3234 guibg=#00aaaa]])
+	vim.api.nvim_set_hl(0, 'PmenuSel',{ fg='#2f3234', bg='#00aaaa' })
 
 end
