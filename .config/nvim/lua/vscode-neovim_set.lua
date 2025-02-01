4bf6c7606702c3d736999b1fcb1809a6e4f11753
tekkamelon <type2guntank@gmail.com>
2025-02-01 17:39:58 +0900

    vim scriptからluaへ書き直し
    設定を"vim.cmd()"内のvim scriptからluaへ書き直し

--- .config/nvim/lua/vscode-neovim_set.lua
+++ .config/nvim/lua/vscode-neovim_set.lua
@@ -1,26 +1,23 @@
 -- vscode-neovim_set.lua
 
 -- "vscode-neovim"の場合のキーマップ
-vim.cmd([[
-	
-	" 保存,終了
-	nnoremap <silent> <leader>w :call VSCodeCall('workbench.action.files.save')<CR>
-	nnoremap <silent> <leader>W :call VSCodeCall('workbench.action.files.saveALL')<CR>
-	nnoremap <silent> <leader>q :call VSCodeCall('workbench.action.closeActiveEditor')<CR>
-	nnoremap <silent> <leader>Q :call VSCodeCall('workbench.action.closeAllEditors')<CR>
 
-	" バッファの切り替え
-	nnoremap <silent> <leader>j :call VSCodeCall('workbench.action.previousEditor')<CR>
-	nnoremap <silent> <leader>k :call VSCodeCall('workbench.action.nextEditor')<CR>
+-- 保存,終了
+vim.keymap.set('n' , '<leader>w' , ":call VSCodeCall('workbench.action.files.save')<CR>" , { noremap = true})
+vim.keymap.set('n' , '<leader>W' , ":call VSCodeCall('workbench.action.files.saveALL')<CR>" , { noremap = true})
+vim.keymap.set('n' , '<leader>q' , ":call VSCodeCall('workbench.action.closeActiveEditor')<CR>" , { noremap = true})
+vim.keymap.set('n' , '<leader>Q' , ":call VSCodeCall('workbench.action.closeAllEditors')<CR>" , { noremap = true})
 
-	" ファイルを開く
-	nnoremap <silent> <leader>ff :call VSCodeCall('workbench.action.files.openFile')<CR>
+-- バッファの切り替え
+vim.keymap.set('n' , '<leader>j' , ":call VSCodeCall('workbench.action.previousEditor')<CR>" , { noremap = true})
+vim.keymap.set('n' , '<leader>k' , ":call VSCodeCall('workbench.action.nextEditor')<CR>" , { noremap = true})
 
-	" フォルダーを開く
-	nnoremap <silent> <leader>fh :call VSCodeCall('workbench.action.files.openFolder')<CR>
+-- ファイルを開く
+vim.keymap.set('n' , '<leader>ff' , ":call VSCodeCall('workbench.action.files.openFile')<CR>" , { noremap = true})
 
-	" ターミナルのトグル
-	nnoremap <silent> <leader>tt :call VSCodeCall('workbench.action.terminal.toggleTerminal')<CR>
+-- フォルダーを開く
+vim.keymap.set('n' , '<leader>fh' , ":call VSCodeCall('workbench.action.files.openFolder')<CR>" , { noremap = true})
 
-]])
+-- ターミナルを起動
+vim.keymap.set('n' , '<leader>tt' , ":call VSCodeCall('workbench.action.terminal.toggleTerminal')<CR>" , { noremap = true})
 
