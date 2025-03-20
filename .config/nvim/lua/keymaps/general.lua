3a94bc92a46cab45e7e2fdd8ad3dedec1f16f373
tekkamelon <type2guntank@gmail.com>
2025-03-21 01:46:27 +0900

    軽微な修正
    テーブルの書式を修正
    for文の一部を修正

--- .config/nvim/lua/keymaps/general.lua
+++ .config/nvim/lua/keymaps/general.lua
@@ -3,7 +3,7 @@
 -- キーマップの設定
 -- ローカル変数を宣言
 local vim_keymap = vim.keymap.set
-local options = { noremap = true }
+local options = {noremap = true}
 
 -- vscode-neovimから起動した際に真,それ以外で偽
 if vim.g.vscode then
@@ -11,30 +11,30 @@ if vim.g.vscode then
 	local kmaps = {
 
 		-- 保存,終了
-		{ '<leader>w' , ":call VSCodeCall('workbench.action.files.save')<CR>" },
-		{ '<leader>W' , ":call VSCodeCall('workbench.action.files.saveALL')<CR>" },
-		{ '<leader>q' , ":call VSCodeCall('workbench.action.closeActiveEditor')<CR>" },
-		{ '<leader>Q' , ":call VSCodeCall('workbench.action.closeAllEditors')<CR>" },
+		{'<leader>w', ":call VSCodeCall('workbench.action.files.save')<CR>"},
+		{'<leader>W', ":call VSCodeCall('workbench.action.files.saveALL')<CR>"},
+		{'<leader>q', ":call VSCodeCall('workbench.action.closeActiveEditor')<CR>"},
+		{'<leader>Q', ":call VSCodeCall('workbench.action.closeAllEditors')<CR>"},
 
 		-- バッファの切り替え
-		{ '<leader>j' , ":call VSCodeCall('workbench.action.previousEditor')<CR>" },
-		{ '<leader>k' , ":call VSCodeCall('workbench.action.nextEditor')<CR>" },
+		{'<leader>j', ":call VSCodeCall('workbench.action.previousEditor')<CR>"},
+		{'<leader>k', ":call VSCodeCall('workbench.action.nextEditor')<CR>"},
 
 		-- ファイルを開く
-		{ '<leader>ff' , ":call VSCodeCall('workbench.action.files.openFile')<CR>" },
+		{'<leader>ff', ":call VSCodeCall('workbench.action.files.openFile')<CR>"},
 
 		-- フォルダーを開く
-		{ '<leader>fh' , ":call VSCodeCall('workbench.action.files.openFolder')<CR>" },
+		{'<leader>fh', ":call VSCodeCall('workbench.action.files.openFolder')<CR>"},
 
 		-- ターミナルを起動
-		{ '<leader>tt' , ":call VSCodeCall('workbench.action.terminal.toggleTerminal')<CR>" },
+		{'<leader>tt', ":call VSCodeCall('workbench.action.terminal.toggleTerminal')<CR>"},
 
 	}
 
 	-- テーブルの内容をループし代入
-	for _, kmaps in pairs(kmaps) do
+	for _, kmap in ipairs(kmaps) do
 
-		vim_keymap('n' , kmaps[1] , kmaps[2] , options)
+		vim_keymap('n', kmap[1], kmap[2], options)
 
 	end
 
@@ -43,27 +43,27 @@ else
 	local kmaps = {
 
 		-- 保存,終了
-		{ 'n' , '<leader>w' , ':w<CR>' , options },
-		{ 'n' , '<leader>W' , ':wq<CR>' , options },
-		{ 'n' , '<leader>q' , ':q<CR>' , options },
-		{ 'n' , '<leader>Q' , ':q!<CR>' , options },
+		{'n', '<leader>w', ':w<CR>'},
+		{'n', '<leader>W', ':wq<CR>'},
+		{'n', '<leader>q', ':q<CR>'},
+		{'n', '<leader>Q', ':q!<CR>'},
 
 		-- バッファの切り替え
-		{ 'n' , '<leader>j' , ':bprev<CR>' , options },
-		{ 'n' , '<leader>k' , ':bnext<CR>' , options },
+		{'n', '<leader>j', ':bprev<CR>'},
+		{'n', '<leader>k', ':bnext<CR>'},
 
 		-- ターミナルノーマルモードへの移行
-		{ 't' , '<C-w><C-n>' , [[<C-\><C-n>]] , options },
+		{'t', '<C-w><C-n>', [[<C-\><C-n>]]},
 
 		-- ビジュアルモード時に"$"で改行を含めないようにする
-		{ 'v' , '$' , 'g_' , {remap = true} },
+		{'v', '$', 'g_', {remap = true}},
 
 	}
 
 	-- テーブルの内容をループし代入
-	for _, kmaps in pairs(kmaps) do
+	for _, kmap in ipairs(kmaps) do
 
-		vim_keymap(kmaps[1] , kmaps[2] , kmaps[3] , kmaps[4])
+		vim_keymap(kmap[1], kmap[2], kmap[3], kmap[4] or options)
 
 	end
 
--- .config/nvim/lua/keymaps/plugins.lua
+++ .config/nvim/lua/keymaps/plugins.lua
@@ -4,7 +4,7 @@
 -- キーマップの設定
 -- ローカル変数を宣言
 local vim_keymap = vim.keymap.set
-local options = { noremap = true }
+local options = {noremap = true, silent = true}
 
 -- vscode-neovimから起動した際に真,それ以外で偽
 if vim.g.vscode then
@@ -13,25 +13,25 @@ if vim.g.vscode then
 
 		-- vim-edgemotion
 		-- 下キーで1つ下のコードブロック
-		{ '<C-j>' , '<Plug>(edgemotion-j)' , options },
-		{ '<C-Down>' , '<Plug>(edgemotion-j)', options },
+		{'<C-j>', '<Plug>(edgemotion-j)'},
+		{'<C-Down>', '<Plug>(edgemotion-j)'},
 		-- 上キーで1つ上のコードブロック
-		{ '<C-l>' , '<Plug>(edgemotion-k)' , options },
-		{ '<C-Up>' , '<Plug>(edgemotion-k)', options },
+		{'<C-l>', '<Plug>(edgemotion-k)'},
+		{'<C-Up>', '<Plug>(edgemotion-k)'},
 
 		-- jumpcursor
-		{ '<leader>h' , '<Plug>(jumpcursor-jump)' , options },
+		{'<leader>h', '<Plug>(jumpcursor-jump)'},
 
 		-- mini.comment
 		-- コメントアウト
-		{ '<leader>g' , 'gcc' , {remap = true }},
+		{'<leader>g', 'gcc', {remap = true}},
 
 	}
 
 	-- テーブルの内容をループし代入
-	for _, kmap in pairs(kmaps) do
+	for _, kmap in ipairs(kmaps) do
 
-		vim_keymap({'n' , 'v'} , kmap[1] , kmap[2] , kmap[3])
+		vim_keymap({'n', 'v'}, kmap[1], kmap[2], kmap[3] or options)
 
 	end
 
@@ -56,7 +56,7 @@ else
 				vim.api.nvim_feedkeys(
 
 					-- キーコードをneovimが理解可能な形式に変換
-					vim.api.nvim_replace_termcodes("<Tab>" , true , false , true) , "n" , false
+					vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false
 
 				)
 
@@ -93,63 +93,63 @@ else
 
 		-- vim-edgemotion
 		-- 下キーで1つ下のコードブロック
-		{ {'n' , 'v'} , '<C-j>' , '<Plug>(edgemotion-j)' , options },
-		{ {'n' , 'v'} , '<C-Down>' , '<Plug>(edgemotion-j)', options },
+		{{'n', 'v'}, '<C-j>', '<Plug>(edgemotion-j)'},
+		{{'n', 'v'}, '<C-Down>', '<Plug>(edgemotion-j)'},
 		-- 上キーで1つ上のコードブロック
-		{ {'n' , 'v'} , '<C-k>' , '<Plug>(edgemotion-k)' , options },
-		{ {'n' , 'v'} , '<C-Up>' , '<Plug>(edgemotion-k)', options },
+		{{'n', 'v'}, '<C-k>', '<Plug>(edgemotion-k)'},
+		{{'n', 'v'}, '<C-Up>', '<Plug>(edgemotion-k)'},
 
 		-- jumpcursor
-		{ {'n' , 'v'} , '<leader>h' , '<Plug>(jumpcursor-jump)' , options },
+		{{'n', 'v'}, '<leader>h', '<Plug>(jumpcursor-jump)'},
 
 		-- mini.comment
 		-- コメントアウト
-		{ {'n' , 'v'} , '<leader>g' , 'gcc' , {remap = true }},
+		{{'n', 'v'}, '<leader>g', 'gcc', {remap = true}},
 
 		-- vim-partedit
 		-- ビジュアルモード時にコマンドを表示
-		{ 'v' , '<leader>e' , ':Partedit -opener new -filetype ' , options },
+		{'v', '<leader>e', ':Partedit -opener new -filetype '},
 
 		-- telescope
 		-- 隠しファイルを含めず検索
-		{ 'n' , '<leader>ff' , ':Telescope find_files hidden=false previewer=false theme=get_dropdown<CR>' , options },
+		{'n', '<leader>ff', ':Telescope find_files hidden=false previewer=false theme=get_dropdown<CR>'},
 		-- 隠しファイルを含め検索
-		{ 'n' , '<leader>fh' , ':Telescope find_files hidden=true previewer=false theme=get_dropdown<CR>' , options },
+		{'n', '<leader>fh', ':Telescope find_files hidden=true previewer=false theme=get_dropdown<CR>'},
 		-- バッファを検索
-		{ 'n' , '<leader>fb' , ':Telescope buffers previewer=false theme=get_dropdown<CR>' , options },
+		{'n', '<leader>fb', ':Telescope buffers previewer=false theme=get_dropdown<CR>'},
 		-- レジスタ一覧を検索
-		{ 'n' , '<leader>fr' , ':Telescope registers<CR>' , options },
+		{'n', '<leader>fr', ':Telescope registers<CR>'},
 		-- ファイル内文字列を検索
 		-- "$ sudo apt install ripgrep -y"で使用可能
-		{ 'n' , '<leader>fg' , ':Telescope live_grep hidden=true previewer=true theme=get_dropdown<CR>' , options },
+		{'n', '<leader>fg', ':Telescope live_grep hidden=true previewer=true theme=get_dropdown<CR>'},
 
 		-- toggleterm
 		-- 下方,右側,フロートウィンドウのターミナルのトグル
-		{ 'n' , '<leader>tt' , ':ToggleTerm size=20 direction=horizontal<CR>' , options },
-		{ 'n' , '<leader>tv' , ':ToggleTerm size=60 direction=vertical<CR>' , options },
-		{ 'n' , '<leader>tf' , ':ToggleTerm direction=float<CR>' , options },
+		{'n', '<leader>tt', ':ToggleTerm size=20 direction=horizontal<CR>'},
+		{'n', '<leader>tv', ':ToggleTerm size=60 direction=vertical<CR>'},
+		{'n', '<leader>tf', ':ToggleTerm direction=float<CR>'},
 		-- ノーマルモード時に現在カーソルのある行を,ビジュアルモード時に選択範囲をターミナルに送る
-		{ 'n' , '<leader>ts' , ':ToggleTermSendCurrentLine<CR>' , options },
-		{ 'v' , '<leader>ts' , ':ToggleTermSendVisualLines<CR>' , options },
+		{'n', '<leader>ts', ':ToggleTermSendCurrentLine<CR>'},
+		{'v', '<leader>ts', ':ToggleTermSendVisualLines<CR>'},
 
 		-- mini.files
 		-- ファイラをトグル
-		{ 'n', '<C-n>', minifiles_toggle, { noremap = true, silent = true } },
+		{'n', '<C-n>', minifiles_toggle},
 
 		-- copilot
 		-- チャット用バッファをトグル
-		{ {'n' , 'v'} , '<leader>cc' , ':CopilotChatToggle<CR>' , options },
+		{{'n', 'v'}, '<leader>cc', ':CopilotChatToggle<CR>'},
 		-- チャットをリセット
-		{ {'n' , 'v'} , '<leader>cr' , ':CopilotChatReset<CR>' , options },
+		{{'n', 'v'}, '<leader>cr', ':CopilotChatReset<CR>'},
 		-- 言語モデルを変更
-		{ {'n' , 'v'} , '<leader>cm' , ':CopilotChatModels<CR>' , options },
+		{{'n', 'v'}, '<leader>cm', ':CopilotChatModels<CR>'},
 
 	}
 
 	-- テーブルの内容をループし代入
-	for _, kmap in pairs(kmaps) do
+	for _, kmap in ipairs(kmaps) do
 
-		vim_keymap(kmap[1] , kmap[2] , kmap[3] , kmap[4])
+		vim_keymap(kmap[1], kmap[2], kmap[3], options)
 
 	end
 
