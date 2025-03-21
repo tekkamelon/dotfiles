78a3bd00270bf670e1dc673ec8e4620229514a59
tekkamelon <type2guntank@gmail.com>
2025-03-21 18:48:41 +0900

    treesitterを修正
    カラースキームが"industry"の時に設定が正しく機能しないバグを修正

--- .config/nvim/after/plugin/after_set.lua
+++ .config/nvim/after/plugin/after_set.lua
@@ -45,3 +45,36 @@ end
 vim.api.nvim_set_hl(0, 'EyelinerPrimary', {fg = 'red', bold = true, underline = true})
 vim.api.nvim_set_hl(0, 'EyelinerSecondary', {fg = 'orange', bold = true, underline = true})
 
+-- カラースキームが"industry"であれば真
+if vim.g.colors_name == "industry" then
+
+	local ts_start = vim.treesitter.start
+
+	-- 除外するfiletype
+	local excluded_types = {
+
+		help = true,
+		bash = true,
+		awk = true,
+		html = true
+
+	}
+
+	-- 関数"vim.treesitter.start"をオーバーライド
+	vim.treesitter.start = function(bufnr, lang)
+
+		-- 現在のfiletypeを取得
+		local ft = vim.bo[bufnr or 0].filetype
+
+		-- 除外するfiletypeが含まれていれば真
+		if excluded_types[ft] or excluded_types[lang] then
+
+			return
+
+		end
+
+		return ts_start(bufnr, lang)
+
+	end
+
+end
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,5 @@
 -- treesitter.lua
 
-
 -- vscodeから起動していなければ真
 if not vim.g.vscode then
 
@@ -15,38 +14,5 @@ if not vim.g.vscode then
 
 	}
 
-	-- カラースキームが"industry"であれば真
-	if vim.g.colors_name == "industry" then
-
-		local ts_start = vim.treesitter.start
-
-		-- 除外するfiletype
-		local excluded_types = {
-
-			help = true,
-			bash = true,
-			awk = true,
-			html = true
-
-		}
-
-		-- 関数"vim.treesitter.start"をオーバーライド
-		vim.treesitter.start = function(bufnr, lang)
-
-			-- 現在のfiletypeを取得
-			local ft = vim.bo[bufnr or 0].filetype
-
-			-- 除外するfiletypeが含まれていれば真
-			if excluded_types[ft] or excluded_types[lang] then
-
-				return
-
-			end
-
-			return ts_start(bufnr, lang)
-
-		end
-
-	end
-
 end
+
