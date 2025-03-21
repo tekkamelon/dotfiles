326118acf88fc5ca21e2f271697d1caa422f8bfc
tekkamelon <type2guntank@gmail.com>
2025-03-21 16:59:38 +0900

    処理を修正
    カラースキームに応じたポップアップメニューの設定をテーブルに修正

--- .config/nvim/after/plugin/after_set.lua
+++ .config/nvim/after/plugin/after_set.lua
@@ -2,64 +2,76 @@
 -- プラグインの起動後に読込
 
 
--- ポップアップメニューの設定
-vim.api.nvim_set_hl(0, 'Pmenu',{ guifg=white , guibg=grey})
-
 -- 改行時の自動コメントアウトをoff
 vim.api.nvim_create_autocmd('FileType' , {pattern = '*' , command = 'setlocal formatoptions-=ro',})
 
 -- カーソルラインをアンダーラインに設定
 vim.api.nvim_set_hl(0, 'CursorLine' , { underline = true })
 
+-- カラースキームごとのポップアップメニューの設定
+local colorscheme_settings = {
+
+    iceberg = {PmenuSel = {fg = '#2f3234', bg = '#8389a3'}},
+    industry = {PmenuSel = {fg = '#2f3234', bg = '#00aaaa'}},
+
+}
+
 -- ローカル変数"colorscheme"に現在のカラースキームを代入"
-local colorscheme = vim.g.colors_name
+local settings = colorscheme_settings[vim.g.colors_name]
 
--- "colorscheme"が"iceberg"であれば真
-if colorscheme == "iceberg" then
+	if settings then
 
-	vim.api.nvim_set_hl(0, 'PmenuSel',{ fg='#2f3234' , bg='#8389a3' })
+	-- テーブルから現在のハイライトを呼び出す
+    for group, colors in pairs(settings) do
 
--- "colorscheme"が"industry"であれば真
-elseif colorscheme == "industry" then
+        vim.api.nvim_set_hl(0, group, colors)
 
-	vim.api.nvim_set_hl(0, 'PmenuSel',{ fg='#2f3234' , bg='#00aaaa' })
+    end
 
-	-- 特定の言語でハイライトしないようにする
-	vim.treesitter.start = (function(wrapped)
+end
 
-		-- ラッパー関数
-		return function(bufnr, lang)
 
-			-- ローカル変数"ft"にバッファのファイルタイプを代入
-			local ft = vim.fn.getbufvar(bufnr or vim.fn.bufnr(''), '&filetype')
+-- treesitterの設定
+-- カラースキームが"industry"であれば真
+if vim.g.colors_name == "industry" then
 
-			-- 除外する言語のリスト
-			local check = (
+    local ts_start = vim.treesitter.start
 
-				ft == 'help'
-				or lang == 'bash'
-				or lang == 'awk'
-				or lang == 'html'
+	-- 除外するfiletype
+    local excluded_types = {
 
-			)
+        help = true,
+        bash = true,
+        awk = true,
+        html = true
 
-			-- "check"が真であれば終了
-			if check then
+    }
 
-				return
+	-- 関数"vim.treesitter.start"をオーバーライド
+    vim.treesitter.start = function(bufnr, lang)
 
-			end
+		-- 現在のfiletypeを取得
+        local ft = vim.bo[bufnr or 0].filetype
 
-			wrapped(bufnr, lang)
+		-- 除外するfiletypeが含まれていれば真
+        if excluded_types[ft] or excluded_types[lang] then
 
-		end
+            return
 
-	end)(vim.treesitter.start)
+        end
+
+        return ts_start(bufnr, lang)
+
+    end
 
 
 end
 
+
+-- ポップアップメニューの設定
+vim.api.nvim_set_hl(0, 'Pmenu', {fg = '#FFFFFF', bg = '#2f3234'})
+
 -- eyelinerのハイライトの色を設定
-vim.api.nvim_set_hl(0, 'EyelinerPrimary',{ fg='red', bold = true, underline = true })
-vim.api.nvim_set_hl(0, 'EyelinerSecondary',{ fg='orange', bold = true, underline = true })
+vim.api.nvim_set_hl(0, 'EyelinerPrimary', {fg = 'red', bold = true, underline = true})
+vim.api.nvim_set_hl(0, 'EyelinerSecondary', {fg = 'orange', bold = true, underline = true})
 
