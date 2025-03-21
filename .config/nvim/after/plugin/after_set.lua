23a663b7635d440eee9862fa258d0143d792e5bc
tekkamelon <type2guntank@gmail.com>
2025-03-21 17:24:16 +0900

    treesitterの設定を削除
    記述ファイルの移行のために削除

--- .config/nvim/after/plugin/after_set.lua
+++ .config/nvim/after/plugin/after_set.lua
@@ -30,44 +30,6 @@ local settings = colorscheme_settings[vim.g.colors_name]
 
 end
 
-
--- treesitterの設定
--- カラースキームが"industry"であれば真
-if vim.g.colors_name == "industry" then
-
-    local ts_start = vim.treesitter.start
-
-	-- 除外するfiletype
-    local excluded_types = {
-
-        help = true,
-        bash = true,
-        awk = true,
-        html = true
-
-    }
-
-	-- 関数"vim.treesitter.start"をオーバーライド
-    vim.treesitter.start = function(bufnr, lang)
-
-		-- 現在のfiletypeを取得
-        local ft = vim.bo[bufnr or 0].filetype
-
-		-- 除外するfiletypeが含まれていれば真
-        if excluded_types[ft] or excluded_types[lang] then
-
-            return
-
-        end
-
-        return ts_start(bufnr, lang)
-
-    end
-
-
-end
-
-
 -- ポップアップメニューの設定
 vim.api.nvim_set_hl(0, 'Pmenu', {fg = '#FFFFFF', bg = '#2f3234'})
 
