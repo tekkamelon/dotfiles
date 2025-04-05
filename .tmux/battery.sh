dda9f4f8deff306504ce4088fc58bc4d6064090c
tekkamelon <type2guntank@gmail.com>
2025-04-06 00:07:11 +0900

    軽微な修正
    分岐の処理を修正

--- .tmux/battery.sh
+++ .tmux/battery.sh
@@ -4,34 +4,25 @@ set -eu
 
 # ====== 変数の設定 ======
 # ロケールの設定
-export LC_ALL=C
-export LANG=C
+export LC_ALL=C LANG=C
 
 # GNU coreutilsの挙動をPOSIXに準拠
 export POSIXLY_CORRECT=1
+# ====== 変数の設定ここまで ======
 
 
 # バッテリーの有無を確認,あれば真
-if [ -e /sys/class/power_supply/BAT0/capacity ] ; then
+# BAT0,BAT1の有無を確認,残量を取得
+if bat0_status=$(cat /sys/class/power_supply/BAT0/capacity) && bat1_status=$(cat /sys/class/power_supply/BAT1/capacity) ; then
 
-	# BAT0の残量を取得
-	bat0_status=$(cat /sys/class/power_supply/BAT0/capacity)
+	# BAT0とBAT1の値を表示
+	printf '<🔋0:%s%%/🔋1:%s%%>\n' "${bat0_status}" "${bat1_status}"
 
-	# /sys/class/power_supply/BAT1/capacityがあれば真
-	if [ -e /sys/class/power_supply/BAT1/capacity ]; then
+# BAT0の有無を確認,残量を取得
+elif bat0_status=$(cat /sys/class/power_supply/BAT0/capacity) ; then
 
-		# BAT1の残量を取得
-		bat1_status=$(cat /sys/class/power_supply/BAT1/capacity)
-
-		# BAT0とBAT1の値を表示
-		printf '<🔋0:%s%%/🔋1:%s%%>\n' "${bat0_status}" "${bat1_status}"
-
-	else
-
-		# BAT0の値のみ表示
-		printf '<🔋:%s%%>\n' "${bat0_status}"
-
-	fi
+	# BAT0の値のみ表示
+	printf '<🔋:%s%%>\n' "${bat0_status}"
 
 else
 
