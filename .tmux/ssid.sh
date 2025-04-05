342b1f0a0a4bcfd7c36bd5e2da8fa0aaf3966c45
tekkamelon <type2guntank@gmail.com>
2025-04-05 23:26:17 +0900

    軽微な修正
    分岐の処理を修正

--- .tmux/ssid.sh
+++ .tmux/ssid.sh
@@ -4,22 +4,18 @@ set -u
 
 # ====== 変数の設定 ======
 # ロケールの設定
-export LC_ALL=C
-export LANG=C
+export LC_ALL=C LANG=C
 
 # GNU coreutilsの挙動をPOSIXに準拠
 export POSIXLY_CORRECT=1
 # ====== 変数の設定ここまで ======
 
 
-# 現在のssidを変数に代入,wi-fiに接続している場合はssidが表示される
-ssid_text=$(iw dev | grep -F "ssid")
+# 現在のssidを変数に代入,wi-fiに接続している場合はssidを表示
+if ssid_text=$(iw dev | grep -F "ssid") ; then
 
-# ssid_textからssidを抽出
-clean_ssid="${ssid_text#*ssid }"
-
-# ssid_textがあれば真
-if [ -n "${ssid_text}" ] ; then
+	# ssid_textからssidを抽出
+	clean_ssid="${ssid_text#*ssid }"
 
 	# 現在のssidを表示
 	printf '<%s>\n' "${clean_ssid}"
