56c2fdbd44f2c8565e101844f7fbc1a279432d07
tekkamelon <type2guntank@gmail.com>
2025-04-15 04:02:58 +0900

    設定ファイルを移行
    "nvim-hardline"を別ファイルへ移行

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -24,33 +24,7 @@ require('jetpack.packer').add {
 			-- vscode以外から起動した場合に真
 			if not vim.g.vscode then
 
-				require('hardline').setup{
-
-					bufferline = true,
-
-					-- テーマの設定
-					theme = 'one',
-
-					sections = {
-
-						-- 現在のモード
-						{class = 'mode' , item = require('hardline.parts.mode').get_item},
-
-						-- ファイルの種類
-						{class = 'high' , item = require('hardline.parts.filetype').get_item, hide = 60},
-
-						-- カレントバッファのパス
-						{class = 'med' , item = require('hardline.parts.filename').get_item},
-
-						-- セパレーター
-						{class = 'med' , item = '%='},
-
-						-- カレント行の位置
-						{class = 'mode' , item = require('hardline.parts.line').get_item},
-
-					}
-
-				}
+				require('plugins.hardline')
 
 			end
 
---
+++ .config/nvim/lua/plugins/hardline.lua
@@ -0,0 +1,30 @@
+-- hardline.lua
+
+
+require('hardline').setup{
+
+	bufferline = true,
+
+	-- テーマの設定
+	theme = 'one',
+
+	sections = {
+
+		-- 現在のモード
+		{class = 'mode' , item = require('hardline.parts.mode').get_item},
+
+		-- ファイルの種類
+		{class = 'high' , item = require('hardline.parts.filetype').get_item, hide = 60},
+
+		-- カレントバッファのパス
+		{class = 'med' , item = require('hardline.parts.filename').get_item},
+
+		-- セパレーター
+		{class = 'med' , item = '%='},
+
+		-- カレント行の位置
+		{class = 'mode' , item = require('hardline.parts.line').get_item},
+
+	}
+
+}
