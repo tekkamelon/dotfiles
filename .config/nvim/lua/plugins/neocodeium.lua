b8d8178eb725e9f5778891f3db380ad32fd1f4e9
tekkamelon <type2guntank@gmail.com>
2025-06-08 02:14:47 +0900

    設定ファイルを追加
    neocodeiumの設定ファイルを追加

---
+++ .config/nvim/lua/plugins/neocodeium.lua
@@ -0,0 +1,33 @@
+-- neocodeium.lua
+
+
+-- vscodeから起動していなければ真
+if not vim.g.vscode then
+
+	local neocodeium = require("neocodeium")
+
+    neocodeium.setup()
+
+	-- キーマッピングの設定
+	-- 単語単位のサジェストの受け入れ
+	vim.keymap.set("i", "<C-s>", neocodeium.accept_word)
+
+	-- サジェストの受け入れ
+    vim.keymap.set("i", "<Tab>", neocodeium.accept)
+
+	-- 次の候補
+    vim.keymap.set("i", "<C-f>", function()
+
+      neocodeium.cycle(1)
+
+    end)
+
+	-- 前の候補
+    vim.keymap.set("i", "<C-F>", function()
+
+      neocodeium.cycle(-1)
+
+    end)
+
+end
+
