3b28c036a470032b8c1bb6e20337ffde951780ed
tekkamelon <type2guntank@gmail.com>
2025-06-13 02:07:15 +0900

    プラグインを追加
    "supermaven-nvim"を追加
    ただし設定ファイルの配置のみ,プラグイン自体のインストールは行わない

---
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,37 @@
+-- supermaven.lua
+
+
+-- vscodeから起動していなければ真
+if not vim.g.vscode then
+
+	require('supermaven-nvim').setup{
+
+		-- キーマッピングの設定
+		keymaps = {
+
+			accept_suggestion = "<Tab>",
+			clear_suggestion = "<C-q>",
+			accept_word = "<C-s>",
+			clear_word = "<C-y>",
+
+		},
+
+		color = {
+
+			suggestion_color = "#FFFFFF",
+			cterm = 244,
+
+		},
+
+		-- 除外するファイルのパターン
+		ignore_filetypes = {
+
+			"gitcommit",
+			"markdown",
+
+		},
+
+	}
+
+end
+
