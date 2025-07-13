d3083cd108f7ee7e2a420fc77efd5513f8bdae93
tekkamelon <type2guntank@gmail.com>
2025-07-13 21:10:32 +0900

    "noice.nvim"の修正
    アイコンの文字化けを修正

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -63,9 +63,25 @@ require('jetpack.packer').add {
 
 					notify = {
 
-
 						enabled = true,
 						view = "mini",
+
+					},
+
+					cmdline = {
+
+						enabled = true,
+
+						format = {
+
+							-- 各種プロンプトの設定
+							cmdline = { pattern = "^:", icon = ">", lang = "vim" },
+							search_down = { kind = "search", pattern = "^/", icon = "🔎 /", lang = "regex" },
+							search_up = { kind = "search", pattern = "^%?", icon = "🔍 ?", lang = "regex" },
+							lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "🌙 ", lang = "lua" },
+							help = { pattern = "^:%s*he?l?p?%s+", icon = "📖" },
+
+						},
 
 					},
 
