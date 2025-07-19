ab60416450d526a7ee054e1fba9c393d8b15bb49
tekkamelon <type2guntank@gmail.com>
2025-07-20 00:49:59 +0900

    アダプタを変更
    アダプタをcopilotからopenrouterへ変更

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -5,17 +5,60 @@ if not vim.g.vscode then
 
 	require('codecompanion').setup{
 
+		opts = {
+
+			language = "Japanese",
+
+		},
+
+		window = {
+
+			layout = "float", -- vertical|horizontal|buffer
+			-- position = "bottom",   -- 明示的に下部に配置
+
+		},
+
+		display = {
+
+			chat = {
+
+				auto_scroll = false,
+			},
+
+		},
+
 		strategies = {
 
 			chat = {
 
-			  adapter = "copilot",
+				adapter = "openrouter",
+
+				-- デフォルトでバッファを読み取る
+				variables = {
+
+					["buffer"] = {
+
+						opts = {
+
+							default_params = 'watch', -- or 'pin'
+
+						},
+
+					},
+
+				},
+
+			},
+
+			agent = {
+
+				adapter = "openrouter",
 
 			},
 
 			inline = {
 
-			  adapter = "copilot",
+			  adapter = "openrouter",
 
 			},
 
@@ -24,20 +67,35 @@ if not vim.g.vscode then
 		adapters = {
 
 			-- copilotアダプタを上書き
-			copilot = function()
+			openrouter = function()
 
 			-- 既定のcopilotアダプタをベースに
-			return require("codecompanion.adapters").extend("copilot", {
+			return require("codecompanion.adapters").extend("openai", {
+
+				name = "openrouter",
+				url = "https://openrouter.ai/api/v1/chat/completions",
+
+				env = {
+					api_key = "OPENROUTER_API_KEY",
+				},
+
+				headers = {
+					["HTTP-Referer"] = "https://github.com/olimorris/codecompanion.nvim",
+					["X-Title"] = "codecompanion.nvim",
+				},
+
+				parameters = {
+					stream = true,
+				},
 
 				schema = {
-
 					model = {
-
-					  default = "gpt-4.1",
-
+						-- 使用したいモデルを指定
+						default = "deepseek/deepseek-chat-v3-0324:free",
 					},
 
 				},
+
 			})
 
 			end,
