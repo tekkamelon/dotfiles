9fa96790b8c0f318b8dfff2d0940c67b6ea7de5b
tekkamelon <type2guntank@gmail.com>
2025-07-27 03:40:55 +0900

    軽微な修正
    プロンプトの文字を修正

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -27,7 +27,7 @@ if not vim.g.vscode then
 			format = {
 
 				-- 各種プロンプトの設定
-				cmdline = { pattern = "^:", icon = ">", lang = "vim" },
+				cmdline = { pattern = "^:", icon = ":", lang = "vim" },
 				search_down = { kind = "search", pattern = "^/", icon = "🔎 /", lang = "regex" },
 				search_up = { kind = "search", pattern = "^%?", icon = "🔍 ?", lang = "regex" },
 				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "🌙 ", lang = "lua" },
