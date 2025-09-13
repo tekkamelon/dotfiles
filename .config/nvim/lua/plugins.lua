1329b3f4472bdf4f71fb06097cd2b6aa246a7392
tekkamelon <type2guntank@gmail.com>
2025-09-14 01:27:19 +0900

    feat: AI補完プラグインをneocodeiumに変更
    minuet-ai.nvimからneocodeiumに再度移行

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -120,34 +120,34 @@ require('jetpack.packer').add {
 	 },
 
 	-- neocodeiumの設定
-	-- {'monkoose/neocodeium',
+	{'monkoose/neocodeium',
 
-	-- 	tag = 'v1.14.1',
+		tag = 'v1.14.1',
 
-	-- 	cmd = 'NeoCodeium',
-
-	-- 	event = 'InsertEnter',
-
-	-- 	config = function()
-
-	-- 		require('plugins.neocodeium')
-
-	-- 	end,
-
-	-- },
-
-	-- minuetの設定
-	{'milanglacier/minuet-ai.nvim',
+		cmd = 'NeoCodeium',
 
 		event = 'InsertEnter',
 
 		config = function()
 
-			require('plugins.minuet')
+			require('plugins.neocodeium')
 
-		end
+		end,
 
 	},
+
+	-- minuetの設定
+	-- {'milanglacier/minuet-ai.nvim',
+
+	-- 	event = 'InsertEnter',
+
+	-- 	config = function()
+
+	-- 		require('plugins.minuet')
+
+	-- 	end
+
+	-- },
 
 	-- CopilotChatの設定
 	{'CopilotC-Nvim/CopilotChat.nvim',
