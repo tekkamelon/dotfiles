e6077c16f49a2dbed981da772d41e5b281ee04d5
tekkamelon <type2guntank@gmail.com>
2025-10-05 16:56:56 +0900

    feat(nvim): LSPの自動フォーマットと自動補完を有効化
    関連するコードのインデントを修正
    LSPがバッファにアタッチされた際に、以下の機能が有効になるように設定を追加
    
    - `textDocument/completion` をサポートするLSPにおいて自動補完を有効化
    - `textDocument/formatting` をサポートするLSPにおいて保存時の自動フォーマットを有効化

--- .config/nvim/lua/plugins/mason-lsp.lua
+++ .config/nvim/lua/plugins/mason-lsp.lua
@@ -3,7 +3,6 @@
 
 
 if not vim.g.vscode then
-
 	-- lspconfigを読み込み
 	local lspconfig = require('lspconfig')
 
@@ -12,45 +11,62 @@ if not vim.g.vscode then
 
 	-- 診断設定をグローバルに定義
 	vim.diagnostic.config({
-	    virtual_text = {
-	        -- プレフィックスアイコン
-	        prefix = '●',
-	        -- ソースを表示(複数ある場合)
-	        source = "if_many",
-	        -- 深刻度でソート
-	        severity_sort = true,
-	    },
-	    -- フロートウィンドウの設定
-	    float = {
-	        border = 'rounded',
-	        source = true,
-	    },
-	    signs = true,
-	    -- サイン列を表示
-	    underline = true,
-	    -- アンダーライン
-	    update_in_insert = false,
-	    -- インサートモード中は更新せず
-	    severity_sort = true,
+		virtual_text = {
+			-- プレフィックスアイコン
+			prefix = '●',
+			-- ソースを表示(複数ある場合)
+			source = "if_many",
+			-- 深刻度でソート
+			severity_sort = true,
+		},
+		-- フロートウィンドウの設定
+		float = {
+			border = 'rounded',
+			source = true,
+		},
+		signs = true,
+		-- サイン列を表示
+		underline = true,
+		-- アンダーライン
+		update_in_insert = false,
+		-- インサートモード中は更新せず
+		severity_sort = true,
+	})
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("my.lsp", {}),
+		callback = function(args)
+			local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+			-- 補完の設定
+			if client:supports_method('textDocument/completion') then
+				-- 文字を入力する度に補完を表示(遅くなる可能性あり)
+				local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+				client.server_capabilities.completionProvider.triggerCharacters = chars
+				-- 補完を有効化
+				vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+			end
+
+			-- フォーマット
+			if not client:supports_method('textDocument/willSaveWaitUntil')
+				and client:supports_method('textDocument/formatting') then
+				vim.api.nvim_create_autocmd('BufWritePre', {
+					group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+					buffer = args.buf,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 3000 })
+					end,
+				})
+			end
+		end,
 	})
 
 	require('mason-lspconfig').setup({
-
 		handlers = {
-
 			function(server_name)
-
-				lspconfig[server_name].setup{
-
+				lspconfig[server_name].setup {
 					capabilities = capabilities,
-
-					-- 診断設定をここにオーバーライド可能(必要に応じて)
 				}
-
 			end,
-
 		}
-
 	})
-
 end
