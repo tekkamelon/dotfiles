1f3435f874bde0d73e17f57e8275d3a9a3271a34
tekkamelon <type2guntank@gmail.com>
2025-10-05 17:05:06 +0900

    軽微な修正
    インデントなどを修正
    mini.statuslineのLSPなどのハイライトを修正

--- .config/nvim/after/plugin/after_set.lua
+++ .config/nvim/after/plugin/after_set.lua
@@ -3,25 +3,25 @@
 
 
 -- 改行時の自動コメントアウトをoff
-vim.api.nvim_create_autocmd('FileType' , {pattern = '*' , command = 'setlocal formatoptions-=ro',})
+vim.api.nvim_create_autocmd('FileType', { pattern = '*', command = 'setlocal formatoptions-=ro', })
 
 -- カーソルラインをアンダーラインに設定
-vim.api.nvim_set_hl(0, 'CursorLine' , { underline = true })
+vim.api.nvim_set_hl(0, 'CursorLine', { underline = true })
 
 -- カラースキームごとのポップアップメニューの設定
 local colorscheme_settings = {
 
-    iceberg = {
+	iceberg = {
 
-		PmenuSel = {fg = '#2f3234', bg = '#8389a3'},
-		Pmenu = {fg = '#FFFFFF', bg = '#2f3234'}
+		PmenuSel = { fg = '#2f3234', bg = '#8389a3' },
+		Pmenu = { fg = '#FFFFFF', bg = '#2f3234' }
 
 	},
 
-    industry = {
+	industry = {
 
-		PmenuSel = {fg = '#2f3234', bg = '#00aaaa'},
-		Pmenu = {fg = '#FFFFFF', bg = '#000000'}
+		PmenuSel = { fg = '#2f3234', bg = '#00aaaa' },
+		Pmenu = { fg = '#FFFFFF', bg = '#000000' }
 
 	},
 
@@ -30,49 +30,44 @@ local colorscheme_settings = {
 -- ローカル変数"colorscheme"に現在のカラースキームを代入"
 local settings = colorscheme_settings[vim.g.colors_name]
 
-	if settings then
-
+if settings then
 	-- テーブルから現在のハイライトを呼び出す
-    for group, colors in pairs(settings) do
-
-        vim.api.nvim_set_hl(0, group, colors)
-
-    end
-
+	for group, colors in pairs(settings) do
+		vim.api.nvim_set_hl(0, group, colors)
+	end
 end
 
 -- ハイライト設定をテーブルで一括管理
 local highlight_groups = {
-    -- eyelinerのハイライトの色を設定
-    EyelinerPrimary = {fg = 'red', bold = true, underline = true},
-    EyelinerSecondary = {fg = 'orange', bold = true, underline = true},
+	-- eyelinerのハイライトの色を設定
+	EyelinerPrimary = { fg = 'red', bold = true, underline = true },
+	EyelinerSecondary = { fg = 'orange', bold = true, underline = true },
 
-    -- mini.statuslineの設定
-    MiniStatuslineModeNormal = {fg = 'white', bg = 'green', bold = true, underline = true},
-    MiniStatuslineModeInsert = {fg = 'white', bg = 'pink', bold = true, underline = true},
-    MiniStatuslineModeVisual = {fg = 'white', bg = 'darkorange', bold = true, underline = true},
-    MiniStatuslineModeCommand = {fg = 'white', bg = 'blue', bold = true, underline = true},
+	-- mini.statuslineの設定
+	MiniStatuslineModeNormal = { fg = 'white', bg = 'green', bold = true, underline = true },
+	MiniStatuslineModeInsert = { fg = 'white', bg = 'pink', bold = true, underline = true },
+	MiniStatuslineModeVisual = { fg = 'white', bg = 'darkorange', bold = true, underline = true },
+	MiniStatuslineModeCommand = { fg = 'white', bg = 'blue', bold = true, underline = true },
 	-- LSPなどの表示
-    MiniStatuslineDevinfo = {fg = 'white', bg = 'darkblue', bold = true, underline = false},
+	MiniStatuslineDevinfo = { fg = 'white', bg = 'purple', bold = true, underline = false },
 
-    -- mini.tablineの設定
-    MiniTablineCurrent = {fg = 'lime', bg = 'black', bold = true, underline = true},
-    MiniTablineVisible = {fg = 'teal', bg = '#2e3234', bold = true, underline = true},
-    MiniTablineHidden = {fg = 'white', bg = '#2e3234', bold = false, underline = false},
-    MiniTablineModifiedCurrent = {fg = 'red', bg = 'black', bold = true, underline = true},
-    MiniTablineModifiedVisible = {fg = 'orange', bg = '#2e3234', bold = true, underline = true},
-    MiniTablineModifiedHidden = {fg = 'pink', bg = '#2e3234', bold = false, underline = false},
-    MiniStatuslineModeOther = {fg = 'white', bg = 'black', bold = true, underline = false},
+	-- mini.tablineの設定
+	MiniTablineCurrent = { fg = 'lime', bg = 'black', bold = true, underline = true },
+	MiniTablineVisible = { fg = 'teal', bg = '#2e3234', bold = true, underline = true },
+	MiniTablineHidden = { fg = 'white', bg = '#2e3234', bold = false, underline = false },
+	MiniTablineModifiedCurrent = { fg = 'red', bg = 'black', bold = true, underline = true },
+	MiniTablineModifiedVisible = { fg = 'orange', bg = '#2e3234', bold = true, underline = true },
+	MiniTablineModifiedHidden = { fg = 'pink', bg = '#2e3234', bold = false, underline = false },
+	MiniStatuslineModeOther = { fg = 'white', bg = 'black', bold = true, underline = false },
 }
 
 -- テーブルからハイライトを一括設定
 for group, colors in pairs(highlight_groups) do
-    vim.api.nvim_set_hl(0, group, colors)
+	vim.api.nvim_set_hl(0, group, colors)
 end
 
 -- カラースキームが"industry"であれば真
 if vim.g.colors_name == "industry" then
-
 	local ts_start = vim.treesitter.start
 
 	-- 除外するfiletype
@@ -87,19 +82,14 @@ if vim.g.colors_name == "industry" then
 
 	-- 関数"vim.treesitter.start"をオーバーライド
 	vim.treesitter.start = function(bufnr, lang)
-
 		-- 現在のfiletypeを取得
 		local ft = vim.bo[bufnr or 0].filetype
 
 		-- 除外するfiletypeが含まれていれば真
 		if excluded_types[ft] or excluded_types[lang] then
-
 			return
-
 		end
 
 		return ts_start(bufnr, lang)
-
 	end
-
 end
