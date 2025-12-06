1ca80f595564b99f1ea9d9626dafef5d17896c1f
tekkamelon <type2guntank@gmail.com>
2025-12-06 21:32:16 +0900

    feat(nvim): VSCode環境に応じてコマンドラインの高さを変更

--- .config/nvim/light_init.lua
+++ .config/nvim/light_init.lua
@@ -26,9 +26,7 @@ local options = {
 
 -- "options"内の左辺を"let",右辺を"status"にそれぞれ代入しループ
 for let, status in pairs(options) do
-
-  vim.g[let] = status
-
+	vim.g[let] = status
 end
 -- ====== 標準プラグインの読込停止ここまで ======
 
@@ -48,58 +46,64 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 -- 背景色をダークモードに設定
-vim.opt.background = 'dark' 
+vim.opt.background = 'dark'
 
 -- ビジュアルモード時に"$"で改行を含めないようにする
-vim.keymap.set('v' , '$' , 'g_' , {remap = true})
+vim.keymap.set('v', '$', 'g_', { remap = true })
 
 -- 分割方向を下と右に設定
 vim.opt.splitbelow = true
-vim.opt.splitright= true
+vim.opt.splitright = true
 
 -- swapファイルを別ディレクトリに作成
 vim.opt.directory = '/tmp'
 
 -- カーソルラインをアンダーラインに設定
 vim.opt.cursorline = true
-vim.api.nvim_set_hl(0, 'CursorLine' , { underline = true })
+vim.api.nvim_set_hl(0, 'CursorLine', { underline = true })
 
 -- カラースキームをindustryに設定
 vim.cmd([[colorscheme industry]])
 
 
--- ====== ターミナルの設定 ====== 
+-- ====== ターミナルの設定 ======
 -- ターミナルノーマルモードへの移行
-vim.keymap.set('t' , '<C-w><C-n>' , [[<C-\><C-n>]] , {noremap = true})
+vim.keymap.set('t', '<C-w><C-n>', [[<C-\><C-n>]], { noremap = true })
 
 -- ターミナル起動時に行番号を非表示
-vim.api.nvim_create_autocmd('TermOpen' , {pattern = '*' , command = 'setlocal norelativenumber',})
-vim.api.nvim_create_autocmd('TermOpen' , {pattern = '*' , command = 'setlocal nonumber',})
+vim.api.nvim_create_autocmd('TermOpen', { pattern = '*', command = 'setlocal norelativenumber', })
+vim.api.nvim_create_autocmd('TermOpen', { pattern = '*', command = 'setlocal nonumber', })
 
 -- "Bterm"コマンドの設定,ターミナルを下画面に高さを7行分下げた状態で起動
-vim.api.nvim_create_user_command('Bterm' , 'split | resize -7 | terminal', { nargs = 0 })
+vim.api.nvim_create_user_command('Bterm', 'split | resize -7 | terminal', { nargs = 0 })
 
 -- "Vterm"の設定,ターミナルを右半分に起動
-vim.api.nvim_create_user_command('Vterm' , 'vsplit | terminal', { nargs = 0 })
--- ====== ターミナルの設定の終了 ====== 
+vim.api.nvim_create_user_command('Vterm', 'vsplit | terminal', { nargs = 0 })
+-- ====== ターミナルの設定の終了 ======
 
 
--- ====== leaderの設定 ====== 
--- leaderをspaceに設定  
+-- ====== leaderの設定 ======
+-- leaderをspaceに設定
 vim.g.mapleader = " "
 
 -- 保存,終了
-vim.keymap.set('n' , '<leader>w' , ':w<CR>' , {noremap = true})
-vim.keymap.set('n' , '<leader>W' , ':wq<CR>' , {noremap = true})
-vim.keymap.set('n' , '<leader>q' , ':q<CR>' , {noremap = true})
-vim.keymap.set('n' , '<leader>Q' , ':q!<CR>' , {noremap = true})
+vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>W', ':wq<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>Q', ':q!<CR>', { noremap = true })
 
 -- バッファの切り替え
-vim.keymap.set('n' , '<leader>j' , ':bprev<CR>' , {noremap = true})
-vim.keymap.set('n' , '<leader>k' , ':bnext<CR>' , {noremap = true})
+vim.keymap.set('n', '<leader>j', ':bprev<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>k', ':bnext<CR>', { noremap = true })
 
 -- ターミナルの起動
-vim.keymap.set('n' , '<leader>tt' , ':Bterm<CR>' , {noremap = true})
-vim.keymap.set('n' , '<leader>tv' , ':Vterm<CR>' , {noremap = true})
--- ====== leaderの設定ここまで ====== 
+vim.keymap.set('n', '<leader>tt', ':Bterm<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>tv', ':Vterm<CR>', { noremap = true })
+-- ====== leaderの設定ここまで ======
 
+-- コマンドラインの高さを設定
+vim.opt.cmdheight = 0
+-- VSCode環境の場合、コマンドラインの高さを1に設定
+if vim.g.vscode then
+	vim.opt.cmdheight = 1
+end
