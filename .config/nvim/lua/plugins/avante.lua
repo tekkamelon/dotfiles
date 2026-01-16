ef62e6dceca3754187b19e03f0d36b25b0236e5f
tekkamelon <type2guntank@gmail.com>
2026-01-16 15:30:54 +0900

    feat(nvim): avanteのカスタムプロンプトを別ファイルに分離

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -69,6 +69,9 @@ local DISABLED_TOOLS = {
 	-- "create_dir"
 }
 
+-- カスタムプロンプトを読み込み
+local shortcuts = require("plugins.avante_shortcuts")
+
 require('avante').setup {
 
 	-- デフォルトのプロバイダ
@@ -175,48 +178,7 @@ require('avante').setup {
 		},
 	},
 
-	shortcuts = {
-		{
-			name = "explain",
-			description = "詳細解説",
-			prompt = "現在のコードを日本語で解説して下さい",
-		},
-		{
-			name = "review",
-			description = "品質レビュー",
-			prompt = "現在のコードを日本語でレビュー,良い点と改善すべき点を指摘してください",
-		},
-		{
-			name = "fix",
-			description = "品質レビュー",
-			prompt = "現在発生しているエラーを修正して下さい",
-		},
-		{
-			name = "refactor",
-			description = "最適化",
-			prompt = "現在のコードを動作や出力を変えず,より効率を向上させたコードに変更して下さい",
-		},
-		{
-			name = "test",
-			description = "テスト追加",
-			prompt = "コードに適切なテストを追加して下さい",
-		},
-		{
-			name = "comment",
-			description = "コメント追加",
-			prompt = "コードに日本語で適切なコメントを入れてください.ただし,コードとコメントの行を分けてください",
-		},
-		{
-			name = "kaisetsu",
-			description = "用語解説",
-			prompt = "選択範囲を日本語かつ簡潔に解説してください",
-		},
-		{
-			name = "matome",
-			description = "各章ごとのまとめ",
-			prompt = "各章ごとの内容をまとめつつ,得られた総合的な知見を最終行以降に記述して下さい",
-		},
-	},
+
 
 	behaviour = {
 		auto_suggestions = false,
@@ -259,4 +221,5 @@ require('avante').setup {
 			require("mcphub.extensions.avante").mcp_tool(),
 		}
 	end,
+	shortcuts = shortcuts,
 }
---
+++ .config/nvim/lua/plugins/avante_shortcuts.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		name = "explain",
+		description = "詳細解説",
+		prompt = "現在のコードを日本語で解説して下さい",
+	},
+	{
+		name = "review",
+		description = "品質レビュー",
+		prompt = "現在のコードを日本語でレビュー,良い点と改善すべき点を指摘してください",
+	},
+	{
+		name = "fix",
+		description = "品質レビュー",
+		prompt = "現在発生しているエラーを修正して下さい",
+	},
+	{
+		name = "refactor",
+		description = "最適化",
+		prompt = "現在のコードを動作や出力を変えず,より効率を向上させたコードに変更して下さい",
+	},
+	{
+		name = "test",
+		description = "テスト追加",
+		prompt = "コードに適切なテストを追加して下さい",
+	},
+	{
+		name = "comment",
+		description = "コメント追加",
+		prompt = "コードに日本語で適切なコメントを入れてください.ただし,コードとコメントの行を分けてください",
+	},
+	{
+		name = "kaisetsu",
+		description = "用語解説",
+		prompt = "選択範囲を日本語かつ簡潔に解説してください",
+	},
+	{
+		name = "matome",
+		description = "各章ごとのまとめ",
+		prompt = "各章ごとの内容をまとめつつ,得られた総合的な知見を最終行以降に記述して下さい",
+	},
+}
