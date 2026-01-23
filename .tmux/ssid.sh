1c0b823e9020d4fec53d2997115dbe85dfaa332d
tekkamelon <type2guntank@gmail.com>
2026-01-23 13:44:27 +0900

    feat(tmux): ssid.shのssid取得にnmcliを追加

--- .tmux/ssid.sh
+++ .tmux/ssid.sh
@@ -12,10 +12,19 @@ export POSIXLY_CORRECT=1
 
 
 # 現在のssidを変数に代入,wi-fiに接続している場合はssidを表示
-if ssid_text=$(iw dev | grep -F "ssid") ; then
+if command -v iw >/dev/null 2>&1 && ssid_text=$(iw dev | grep -F "ssid"); then
 
 	# ssid_textからssidを抽出
 	clean_ssid="${ssid_text#*ssid }"
+
+	# 現在のssidを表示
+	printf '<%s>\n' "${clean_ssid}"
+
+# iwコマンドが利用できない場合はnmcliを実行
+elif command -v nmcli >/dev/null 2>&1 && ssid_text=$(nmcli -t -f active,ssid dev wifi | grep '^yes:' | cut -d':' -f2); then
+
+	# nmcliからssidを抽出
+	clean_ssid="${ssid_text}"
 
 	# 現在のssidを表示
 	printf '<%s>\n' "${clean_ssid}"
